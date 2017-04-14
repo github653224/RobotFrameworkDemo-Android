a820f00eabf0f1cbd41dd593df1b2aeae50ce259
Haejoong Lee <hlee@anexinet.com>
2017-04-14 15:32:00 -0400

    initial imports

---
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
