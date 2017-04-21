5b7dedba7b9641d7507ee6c787c55a3529b103f5
Haejoong Lee <hlee@anexinet.com>
2017-04-21 16:46:19 -0400

    add code for 3rd test case

--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -3,7 +3,10 @@ package com.example.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,7 +17,13 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLogin(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        TextView username = (TextView) findViewById(R.id.txt_username);
+        TextView password = (TextView) findViewById(R.id.txt_password);
+        if (TextUtils.isEmpty(username.getText()) || TextUtils.isEmpty(password.getText())) {
+            Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
